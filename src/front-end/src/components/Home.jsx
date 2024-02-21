2939612db4f0d95c01043baf214aceadcacb91fa
deandresugandhi <deandre_21@hotmail.com>
2024-02-21 09:05:47 +0700

    Temporary commit

--- src/front-end/src/components/Home.jsx
+++ src/front-end/src/components/Home.jsx
@@ -2,20 +2,20 @@ import React from 'react'
 import QueueEntry from './QueueEntry'
 
 const Home = () => {
-  const [categories, setCategories] = useState([])
-  const [entries, setEntries] = useState([])
+  // const [categories, setCategories] = useState([])
+  // const [entries, setEntries] = useState([])
 
-  useEffect(() => {
-      fetch('https://journal-api-deployment.onrender.com/categories')
-      .then(res => res.json())
-      .then(data => setCategories(data))
-  }, [])
+  // useEffect(() => {
+  //     fetch('https://journal-api-deployment.onrender.com/categories')
+  //     .then(res => res.json())
+  //     .then(data => setCategories(data))
+  // }, [])
 
-  useEffect(() => {s
-      fetch('https://journal-api-deployment.onrender.com/entries')
-      .then(res => res.json())
-      .then(data => setEntries(data))
-  }, [])
+  // useEffect(() => {s
+  //     fetch('https://journal-api-deployment.onrender.com/entries')
+  //     .then(res => res.json())
+  //     .then(data => setEntries(data))
+  // }, [])
   return (
     <>
       <div>Home</div>
