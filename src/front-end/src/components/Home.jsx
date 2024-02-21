c954f3baaa51374eb9ba6b69cc44dbe12765c12a
deandresugandhi <deandre_21@hotmail.com>
2024-02-21 08:53:26 +0700

    Added QueueEntry component

--- src/front-end/src/components/Home.jsx
+++ src/front-end/src/components/Home.jsx
@@ -1,8 +1,26 @@
 import React from 'react'
+import QueueEntry from './QueueEntry'
 
 const Home = () => {
+  const [categories, setCategories] = useState([])
+  const [entries, setEntries] = useState([])
+
+  useEffect(() => {
+      fetch('https://journal-api-deployment.onrender.com/categories')
+      .then(res => res.json())
+      .then(data => setCategories(data))
+  }, [])
+
+  useEffect(() => {s
+      fetch('https://journal-api-deployment.onrender.com/entries')
+      .then(res => res.json())
+      .then(data => setEntries(data))
+  }, [])
   return (
-    <div>Home</div>
+    <>
+      <div>Home</div>
+      <QueueEntry />
+    </>
   )
 }
 
